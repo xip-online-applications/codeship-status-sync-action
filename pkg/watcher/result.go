cf867ac46b982ed37c1610bf5789531a0e6b81a7
Youri Lefers <youri.lefers@x-ip.nl>
2020-11-25 20:08:42 +0100

    Print build status as a result

--- pkg/watcher/result.go
+++ pkg/watcher/result.go
@@ -31,12 +31,20 @@ func (h *ResultHandler) handle() {
 
 func (h *ResultHandler) success() {
 	fmt.Println(fmt.Sprintf("Succesfully handled the build: %s", h.build.Uuid()))
+
+	h.printStatus(h.build.Status())
 }
 
 func (h *ResultHandler) error() {
 	if h.err != nil {
 		fmt.Println(fmt.Sprintf("Failed to handle the result and got an error: %s", h.err.Error()))
+		h.printStatus("error")
 	} else {
 		fmt.Println(fmt.Sprintf("Failed to handle the result with status: %s", h.build.Status()))
+		h.printStatus(h.build.Status())
 	}
+}
+
+func (h *ResultHandler) printStatus(status string) {
+	fmt.Println(fmt.Sprintf(`::set-output name=status::%s`, status))
 }
